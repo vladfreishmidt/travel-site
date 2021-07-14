ed84e22a9b1acc092ddc9f2e9a328674c8af19bd
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2021-07-14 16:57:07 +0300

    Completed Header styles and functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,14 @@
 import '../styles/styles.css';
+import MobileMenu from './MobileMenu.js ';
 
 if (module.hot) {
   module.hot.accept();
 }
+
+// Create a new instance 
+let mobileMenu = new MobileMenu();
+
+
+
+
+
